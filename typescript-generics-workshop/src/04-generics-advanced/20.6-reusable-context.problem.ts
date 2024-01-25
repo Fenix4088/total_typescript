d9b6fc8dc14fbbd791201901fbf872ea804cefed
Yehor Pliasov <fenix@yehors-macbook-air.home>
2024-01-25 20:21:53 +0100

    05 generics advanced done

--- typescript-generics-workshop/src/04-generics-advanced/20.6-reusable-context.problem.ts
+++ typescript-generics-workshop/src/04-generics-advanced/20.6-reusable-context.problem.ts
@@ -12,11 +12,18 @@ import { CSSProperties } from "react";
  *
  * const useStyled = makeUseStyled<MyTheme>();
  */
-const useStyled = <TTheme = {}>(func: (theme: TTheme) => CSSProperties) => {
-  // Imagine that this function hooks into a global theme
-  // and returns the CSSProperties
-  return {} as CSSProperties;
-};
+
+
+
+const makeStyles = <TTheme = {}>() => {
+  const useStyled = <TTheme = {}>(func: (theme: TTheme) => CSSProperties) => {
+    // Imagine that this function hooks into a global theme
+    // and returns the CSSProperties
+    return {} as CSSProperties;
+  };
+
+  return useStyled<TTheme>
+}
 
 interface MyTheme {
   color: {
@@ -27,11 +34,13 @@ interface MyTheme {
   };
 }
 
-const buttonStyle = useStyled<MyTheme>((theme) => ({
+const useStyled = makeStyles<MyTheme>()
+
+const buttonStyle = useStyled((theme) => ({
   color: theme.color.primary,
   fontSize: theme.fontSize.small,
 }));
 
-const divStyle = useStyled<MyTheme>((theme) => ({
+const divStyle = useStyled((theme) => ({
   backgroundColor: theme.color.primary,
 }));
--- typescript-generics-workshop/src/04-generics-advanced/20.7-working-around-partial-inference.problem.ts
+++ typescript-generics-workshop/src/04-generics-advanced/20.7-working-around-partial-inference.problem.ts
@@ -1,13 +1,18 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const makeSelectors = <
-  TSource,
-  TSelectors extends Record<string, (source: TSource) => any>,
->(
-  selectors: TSelectors,
-) => {
+// export const makeSelectors = <
+//   TSource,
+//   TSelectors extends Record<string, (source: TSource) => any>,
+// >(
+//   selectors: TSelectors,
+// ) => {
+//   return selectors;
+// };
+
+
+export const makeSelectors = <TSource>() => <TSelectors extends Record<string, (source: TSource) => any>>( selectors: TSelectors) => {
   return selectors;
-};
+}
 
 interface Source {
   firstName: string;
@@ -27,12 +32,20 @@ interface Source {
  *
  * makeSelectors<Source>()({ ...selectorsGoHere })
  */
-const selectors = makeSelectors<Source>({
+
+const selectors = makeSelectors<Source>()({
   getFullName: (source) =>
     `${source.firstName} ${source.middleName} ${source.lastName}`,
   getFirstAndLastName: (source) => `${source.firstName} ${source.lastName}`,
   getFirstNameLength: (source) => source.firstName.length,
-});
+})
+
+// const selectors = makeSelectors<Source>({
+//   getFullName: (source) =>
+//     `${source.firstName} ${source.middleName} ${source.lastName}`,
+//   getFirstAndLastName: (source) => `${source.firstName} ${source.lastName}`,
+//   getFirstNameLength: (source) => source.firstName.length,
+// });
 
 type tests = [
   Expect<Equal<(typeof selectors)["getFullName"], (source: Source) => string>>,
